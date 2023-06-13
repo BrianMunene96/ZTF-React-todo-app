6f16912deb1f200a8184420a27507ab67e43719f
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-06-13 16:57:16 +0200

    enable adding a todo

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodos = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  };
+
+  const deleteTodos = (selectedTodo) => {
+    const deleted = todos.filter((todo) => todo !== selectedTodo);
+    setTodos(deleted);
+  };
+
   return (
     <div className="todo__container">
       <div className="todo__form">
         <form action="">
-          <input className="todo__form__input" type="text" />
-          <button className="todo__form__button">Add Todo</button>
+          <input
+            className="todo__form__input"
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <button
+            type="submit"
+            className="todo__form__button"
+            onClick={addTodos}
+          >
+            Add Todo
+          </button>
         </form>
       </div>
+
       <ul className="todo__lists">
-        <li className="todo__list">Buy Milk</li>
-        <i class="gg-close-r"></i>
-        <li className="todo__list">
-          Take the trash out <i class="gg-close-r"></i>
-        </li>
+        {todos.length ? (
+          todos.map((todo) => {
+            return (
+              <li className="todo__list">
+                {todo}{' '}
+                <i
+                  class="fa-solid fa-circle-check"
+                  onClick={() => deleteTodos(todo)}
+                ></i>
+              </li>
+            );
+          })
+        ) : (
+          <h3>You have no todos 😊 Please add a task</h3>
+        )}
       </ul>
     </div>
   );
