c4d3e88d34d1b9508597a6c386862693383492ed
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-06-26 15:35:21 +0200

    add delete and completed buttons

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -7,13 +7,26 @@ const TodoList = () => {
 
   const addTodos = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
+    setTodos([...todos, { text: input, completed: false }]);
     setInput('');
   };
 
   const deleteTodos = (selectedTodo) => {
     const deleted = todos.filter((todo) => todo !== selectedTodo);
     setTodos(deleted);
+  };
+
+  const handleClick = (event) => {
+    if (
+      event.currentTarget.style.color &&
+      event.target.parentNode.style.color
+    ) {
+      event.currentTarget.style.color = null;
+      event.target.parentNode.style.color = null;
+    } else {
+      event.currentTarget.style.color = 'rgb(29, 225, 29)';
+      event.target.parentNode.style.color = 'rgb(29, 225, 29)';
+    }
   };
 
   return (
@@ -36,23 +49,52 @@ const TodoList = () => {
         </form>
       </div>
 
-      <ul className="todo__lists">
+      <div className="todo__lists">
         {todos.length ? (
           todos.map((todo) => {
             return (
-              <li className="todo__list">
-                {todo}{' '}
+              <div className="todo__list">
+                <p>
+                  {todo.text.toUpperCase()}{' '}
+                  <i class="fa-solid fa-circle-check" onClick={handleClick}></i>
+                </p>
+                <div className="todo__buttons">
+                  <button onClick={() => deleteTodos(todo)}>Delete</button>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h3 className="alternative__text">
+            You have no todos 😊 Please add a task
+          </h3>
+        )}
+      </div>
+
+      {/* <ul className="todo__lists">
+        {todos.length ? (
+          todos.map((todo) => {
+            return (
+              <li
+                className={
+                  isComplete ? 'todo__list completed__list' : 'todo__list'
+                }
+              >
+                {todo.text}{' '}
                 <i
                   class="fa-solid fa-circle-check"
-                  onClick={() => deleteTodos(todo)}
+                  // onClick={() => deleteTodos(todo)}
+                  onClick={handleClick}
                 ></i>
               </li>
             );
           })
         ) : (
-          <h3>You have no todos 😊 Please add a task</h3>
+          <h3 className="alternative__text">
+            You have no todos 😊 Please add a task
+          </h3>
         )}
-      </ul>
+      </ul> */}
     </div>
   );
 };
